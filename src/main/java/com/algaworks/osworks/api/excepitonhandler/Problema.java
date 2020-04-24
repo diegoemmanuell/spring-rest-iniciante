89c6da775082842f51676be38acf1322f3d92f46
Diego <diego2013ads@gmail.com>
2020-04-23 21:53:21 -0300

    Enviando para o git

---
+++ src/main/java/com/algaworks/osworks/api/excepitonhandler/Problema.java
@@ -0,0 +1,33 @@
+package com.algaworks.osworks.api.excepitonhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonInclude(Include.NON_NULL)
+public class Problema {
+
+	
+	private @Getter @Setter Integer status;
+	private @Getter @Setter LocalDateTime dataHora;
+	private @Getter @Setter String titulo;
+	private @Getter @Setter List<Campo> campos;
+	
+	public static class Campo{
+		
+		
+		
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		private @Getter @Setter String nome;
+		private @Getter @Setter String mensagem;
+	}
+}
